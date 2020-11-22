9620720a2ddb5f2ddb411014260182d1863697ac
Qt君 <2088201923@qq.com>
2020-11-22 14:32:16 +0800

    重新调整main.cpp

--- QtQuickDemoForAll/main.cpp
+++ QtQuickDemoForAll/main.cpp
@@ -1,49 +1,23 @@
-#include <QtGlobal>
-#if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
-
 #include <QApplication>
-#include <QDeclarativeView>
 
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
+#define IS_QT_4 (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
 
-    QDeclarativeView viewer;
-    viewer.setSource(QUrl::fromLocalFile(QString("%1/%2").arg(MY_QML_DIR).arg("main.qml")));
-    viewer.show();
-    return app.exec();
-}
-
-#else
-
-#include <QApplication>
-#include <QQuickView>
-
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
-
-    QQuickView viewer;
-    viewer.setSource(QUrl::fromLocalFile(QString("%1/%2").arg(MY_QML_DIR).arg("main.qml")));
-    viewer.show();
-    return app.exec();
-}
-
-/************************************OR*********************************/
-
-//#include <QGuiApplication>
-//#include <QQmlApplicationEngine>
-
-//int main(int argc, char *argv[])
-//{
-//    QGuiApplication app(argc, argv);
-
-//    QQmlApplicationEngine engine;
-//    engine.load(QUrl::fromLocalFile(QString("%1/%2").arg(MY_QML_DIR).arg("main-qt5.qml")));
-//    if (engine.rootObjects().isEmpty())
-//        return -1;
-
-//    return app.exec();
-//}
-
+#if IS_QT_4 /* Qt4 */
+    #include <QDeclarativeView>
+#else /* Qt5 more */
+    #include <QQuickView>
 #endif
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+#if IS_QT_4
+    QDeclarativeView viewer;
+#else
+    QQuickView viewer;
+#endif
+    viewer.setSource(QUrl::fromLocalFile(QString("%1/%2").arg(MY_QML_DIR).arg("main.qml")));
+    viewer.show();
+    return app.exec();
+}
